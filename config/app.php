1caee9b680d2924bd479540d380ed864839ecdc9
WILFRIDORAMIREZ <wilirm23@gmail.com>
2017-03-23 12:55:49 -0600

    se instalo plantilla adminlte

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         // ...
         Collective\Html\HtmlServiceProvider::class,
         // ...
+        Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
 
         ],
 
@@ -234,6 +235,7 @@ return [
     'Form' => Collective\Html\FormFacade::class,
     'Html' => Collective\Html\HtmlFacade::class,
     // ...
+    'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
 
     ],
 
